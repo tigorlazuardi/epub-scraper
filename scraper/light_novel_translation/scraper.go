5d9163003944ed34d3f33a81da03ea7dd766db03
TigorHutasuhut <tigor.hutasuhut@gmail.com>
2022-03-10 22:13:13 +0700

    feat(scraper): wip for light novel translation

--- scraper/light_novel_translation/scraper.go
+++ scraper/light_novel_translation/scraper.go
@@ -2,6 +2,7 @@ package lightnoveltranslation
 
 import (
 	"context"
+	"errors"
 	"net/http"
 	"sync"
 
@@ -11,18 +12,22 @@ import (
 
 var _ scraper.Scraper = (*LNTScraper)(nil)
 
+const site = "Light Novel Translation"
+
 type LNTScraper struct {
 	bufferSize int
 	client     scraper.Doer
 	wg         *sync.WaitGroup
+	semaphore  chan struct{}
 }
 
 func (lntscraper *LNTScraper) Scrape(ctx context.Context, url string) <-chan scraper.ScrapeData {
 	c := make(chan scraper.ScrapeData, lntscraper.bufferSize)
 
+	lntscraper.wg.Add(1)
 	go func() {
-		lntscraper.wg.Add(1)
-
+		lntscraper.scrapeSite(ctx, url, 0, c)
+		lntscraper.wg.Done()
 		lntscraper.wg.Wait()
 		close(c)
 	}()
@@ -31,28 +36,45 @@ func (lntscraper *LNTScraper) Scrape(ctx context.Context, url string) <-chan scr
 }
 
 func (lntscraper LNTScraper) scrapeSite(ctx context.Context, url string, index int, channel chan<- scraper.ScrapeData) {
+	const selector = "div.entry-content"
+	lntscraper.semaphore <- struct{}{}
 	defer lntscraper.wg.Done()
 	var err error
 	defer func() {
+		<-lntscraper.semaphore
 		if err != nil {
 			channel <- scraper.ScrapeData{Err: err}
 		}
 	}()
-	req, _ := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
-	res, err := lntscraper.client.Do(req)
+
+	logCtx := map[string]interface{}{
+		"url":      url,
+		"index":    index,
+		"selector": selector,
+	}
+
+	doc, err := lntscraper.fetchSite(ctx, url)
 	if err != nil {
 		return
 	}
-	defer res.Body.Close()
 
-	if res.StatusCode >= 300 {
-
+	if next := lntscraper.findNext(doc); next != "" {
+		lntscraper.wg.Add(1)
+		go lntscraper.scrapeSite(ctx, next, index+1, channel)
 	}
 
+	content := doc.Find(selector).Nodes
+	if len(content) == 0 {
+		msg := "current site does not have content"
+		err = scraper.NewScrapeError(site, msg, errors.New(msg), logCtx)
+		return
+	}
+
+	// TODO: Implement further
 }
 
-func (LNTScraper) findNext(sel *goquery.Selection) (url string) {
-	if nodes := sel.Find("p.alignright").Nodes; len(nodes) > 0 {
+func (LNTScraper) findNext(doc *goquery.Document) (url string) {
+	if nodes := doc.Find("p.alignright").Nodes; len(nodes) > 0 {
 		if a := nodes[0].FirstChild; a != nil {
 			for _, attr := range a.Attr {
 				if attr.Key == "href" {
@@ -61,5 +83,32 @@ func (LNTScraper) findNext(sel *goquery.Selection) (url string) {
 			}
 		}
 	}
+	return
+}
+
+func (lntscraper LNTScraper) fetchSite(ctx context.Context, url string) (doc *goquery.Document, err error) {
+	req, _ := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	logCtx := map[string]interface{}{
+		"url":    url,
+		"method": http.MethodGet,
+	}
+	res, err := lntscraper.client.Do(req)
+	if err != nil {
+		msg := "failed to open connection to target website"
+		return nil, scraper.NewScrapeError(site, msg, err, logCtx)
+	}
+	defer res.Body.Close()
+
+	logCtx["status_code"] = res.StatusCode
+	if res.StatusCode >= 400 {
+		msg := "unexpected error code from server"
+		return nil, scraper.NewScrapeError(site, msg, errors.New(msg), logCtx)
+	}
+
+	doc, err = goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		err = scraper.NewScrapeError(site, "failed to parse website to html", err, logCtx)
+	}
+
 	return
 }
