b93cef5bf7e0799f0e04f6e84d8a71fffdc9f540
tigorhutasuhut <tigor.hutasuhut@gmail.com>
2022-03-10 21:22:45 +0700

    feat(scraper): skeleton update

--- scraper/light_novel_translation/scraper.go
+++ scraper/light_novel_translation/scraper.go
@@ -2,7 +2,10 @@ package lightnoveltranslation
 
 import (
 	"context"
+	"net/http"
+	"sync"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/tigorlazuardi/epub-scraper/scraper"
 )
 
@@ -11,11 +14,52 @@ var _ scraper.Scraper = (*LNTScraper)(nil)
 type LNTScraper struct {
 	bufferSize int
 	client     scraper.Doer
+	wg         *sync.WaitGroup
 }
 
 func (lntscraper *LNTScraper) Scrape(ctx context.Context, url string) <-chan scraper.ScrapeData {
-	panic("not implemented") // TODO: Implement
+	c := make(chan scraper.ScrapeData, lntscraper.bufferSize)
+
+	go func() {
+		lntscraper.wg.Add(1)
+
+		lntscraper.wg.Wait()
+		close(c)
+	}()
+
+	return c
 }
 
-func (lntscraper LNTScraper) scrapeSite(ctx context.Context, url string, channel chan<- scraper.ScrapeData) {
+func (lntscraper LNTScraper) scrapeSite(ctx context.Context, url string, index int, channel chan<- scraper.ScrapeData) {
+	defer lntscraper.wg.Done()
+	var err error
+	defer func() {
+		if err != nil {
+			channel <- scraper.ScrapeData{Err: err}
+		}
+	}()
+	req, _ := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	res, err := lntscraper.client.Do(req)
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode >= 300 {
+
+	}
+
+}
+
+func (LNTScraper) findNext(sel *goquery.Selection) (url string) {
+	if nodes := sel.Find("p.alignright").Nodes; len(nodes) > 0 {
+		if a := nodes[0].FirstChild; a != nil {
+			for _, attr := range a.Attr {
+				if attr.Key == "href" {
+					return attr.Val
+				}
+			}
+		}
+	}
+	return
 }
--- scraper/scraper.go
+++ scraper/scraper.go
@@ -11,6 +11,26 @@ type Doer interface {
 	Do(*http.Request) (*http.Response, error)
 }
 
+type ScrapeError struct {
+	Err     error
+	Message string
+	Context map[string]interface{}
+	Scraper string
+}
+
+func (err ScrapeError) Error() string {
+	return "[" + err.Scraper + "] " + err.Message + ": " + err.Error()
+}
+
+func NewScrapeError(scraper, message string, err error, context map[string]interface{}) error {
+	return ScrapeError{
+		Err:     err,
+		Message: message,
+		Context: context,
+		Scraper: scraper,
+	}
+}
+
 type ScrapeData struct {
 	Index int
 	URL   string
